9abbd07e837e07b4e92d883ecce8f903fcf888a4
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2022-03-15 14:22:36 +0700

    Activity 4 "Membuat menu dan Floating Button pada android"

--- app/src/main/java/com/example/constraintlayoutevenbutton/MainActivity.java
+++ app/src/main/java/com/example/constraintlayoutevenbutton/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.constraintlayoutevenbutton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
     //Deklarasi variabel untuk menyimpan email dan password
     String nama, password;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //method untuk memanggil activity "DaftarActivity"
+            Intent i = new Intent(getApplicationContext(), FormDaftar.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,8 +92,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-}
+    }}
+
+
 
 
 
