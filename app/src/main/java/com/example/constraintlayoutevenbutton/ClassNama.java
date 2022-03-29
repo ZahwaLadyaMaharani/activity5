fc9f5ee9168fd8783124ff04dc50a406dce7726f
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2022-03-29 15:02:31 +0700

    Activity 5

---
+++ app/src/main/java/com/example/constraintlayoutevenbutton/ClassNama.java
@@ -0,0 +1,18 @@
+package com.example.constraintlayoutevenbutton;
+
+public class ClassNama {
+    private String Nama;
+
+    //inisialisasi objek
+    public ClassNama(String nama)
+    {
+        this.Nama = nama;
+    }
+
+    //membuat method getName untuk mengembalikan nilai ke variabel Nama
+
+    public String getNama()
+    {
+        return this.Nama;
+    }
+}
